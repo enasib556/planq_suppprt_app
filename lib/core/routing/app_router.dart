e1e301e3bcad45fbec178c93c4d6d52762370116
enas ibrahim <enasib556.com>
2025-06-30 22:21:40 +0300

    updata 2

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:windows_desktop_app/core/routing/routes.dart';
-import '../../features/splash/splash_screen.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     //this arguments to be passed in any screen like this ( arguments as ClassName )
     final arguments = settings.arguments;
 
-    switch (settings.name) {
-      case Routes.splashScreen:
-        return MaterialPageRoute(
-          builder: (_) => const SplashScreen(),
-        );
-      default:
-        return null;
-    }
+    // switch (settings.name) {
+    //   case Routes.splashScreen:
+    //     return MaterialPageRoute(
+    //       builder: (_) => const SplashScreen(),
+    //     );
+    //   default:
+    //     return null;
+    // }
   }
 }
