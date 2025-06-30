c9a51ad70a6e7812688c3fcb41cfdf3648c055d1
enas ibrahim <enasib556.com>
2025-06-30 08:40:10 +0300

    updata

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'core/routing/app_router.dart';
-import 'quran_app.dart';
+import 'planq_support_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
