e1ba177fad92e245c18e75ed8c94eedd9d8798cc
Your Name <your-email@example.com>
2025-07-12 21:53:11 +0300

    Add mouse and BIOS routes to the application routing system

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -2,4 +2,6 @@ class Routes {
   static const String HomeScreen = '/HomeScreen';
   static const String batteryScreen = '/batteryScreen';
   static const String storageScreen = '/storageScreen';
+  static const String mouseScreen = '/mouseScreen';
+  static const String biosScreen = '/biosScreen';
 }
