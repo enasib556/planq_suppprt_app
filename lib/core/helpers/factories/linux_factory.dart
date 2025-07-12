1b4b6762b3dc89bb7352a1c5bf913a5d147e14e7
Your Name <your-email@example.com>
2025-07-12 21:52:05 +0300

    Add Linux mouse and BIOS service creation methods to LinuxFactory

--- lib/core/helpers/factories/linux_factory.dart
+++ lib/core/helpers/factories/linux_factory.dart
@@ -2,6 +2,10 @@ import '../../../features/battery/domain/battery_service.dart';
 import '../../../features/battery/domain/linux_battery_service.dart';
 import '../../../features/storage/domain/linux_storage_service.dart';
 import '../../../features/storage/domain/storage_service.dart';
+import '../../../features/mouse/domain/mouse_service.dart';
+import '../../../features/mouse/domain/linux_mouse_service.dart';
+import '../../../features/bios/domain/bios_service.dart';
+import '../../../features/bios/domain/linux_bios_service.dart';
 import 'device_factory.dart';
 
 
@@ -11,4 +15,10 @@ class LinuxFactory implements DeviceFactory {
 
   @override
   StorageService createStorageService() => LinuxStorageService();
+
+  @override
+  MouseService createMouseService() => LinuxMouseService();
+
+  @override
+  BIOSService createBIOSService() => LinuxBIOSService();
 }
