ecb75a991e0d9628c107d091686227814e47d4a2
Your Name <your-email@example.com>
2025-07-12 21:51:37 +0300

    Add mouse and BIOS service creation methods to DeviceFactory

--- lib/core/helpers/factories/device_factory.dart
+++ lib/core/helpers/factories/device_factory.dart
@@ -1,8 +1,12 @@
 import '../../../features/battery/domain/battery_service.dart';
 import '../../../features/storage/domain/storage_service.dart';
+import '../../../features/mouse/domain/mouse_service.dart';
+import '../../../features/bios/domain/bios_service.dart';
 
 
 abstract class DeviceFactory {
   BatteryService createBatteryService();
   StorageService createStorageService();
+  MouseService createMouseService();
+  BIOSService createBIOSService();
 }
