1cf6775f78acbb4c199e679b213acfdfe5053c0a
Your Name <your-email@example.com>
2025-07-12 21:52:38 +0300

    Add mouse and BIOS service creation methods to WindowsFactory

--- lib/core/helpers/factories/windows_factory.dart
+++ lib/core/helpers/factories/windows_factory.dart
@@ -3,6 +3,10 @@ import 'package:windows_desktop_app/features/storage/domain/storage_service.dart
 import '../../../features/battery/domain/battery_service.dart';
 import '../../../features/battery/domain/windows_battery_service.dart';
 import '../../../features/storage/domain/windows_storage_service.dart';
+import '../../../features/mouse/domain/mouse_service.dart';
+import '../../../features/mouse/domain/windows_mouse_service.dart';
+import '../../../features/bios/domain/bios_service.dart';
+import '../../../features/bios/domain/windows_bios_service.dart';
 import 'device_factory.dart';
 
 
@@ -12,4 +16,10 @@ class WindowsFactory implements DeviceFactory {
 
   @override
   StorageService createStorageService() => WindowsStorageService();
+
+  @override
+  MouseService createMouseService() => WindowsMouseService();
+
+  @override
+  BIOSService createBIOSService() => WindowsBIOSService();
 }
